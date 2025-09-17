01273e4f524cf77d1eaf3d94a9caa2298908a578
DIOUF-MOR <daaroukhoudos04@gmail.com>
2025-09-17 15:43:33 +0000

    Initial commit

---
+++ src/main/java/com/tawfex/cours_devops/CoursDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.tawfex.cours_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tawfex/cours_devops/CoursDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawfex.cours_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
